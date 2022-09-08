a795ad6cb277bff88e05fda859138b391dc5fb1e
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-08 01:27:21 -0700

    Adopted bettty style for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry Point
  *
- * Description: A simple C program that prints with function puts() 
+ * Description: A simple C program that prints with function puts()
  *
- * Return: 0 */
+ * Return: 0
+*/
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
+return (0);
 }
