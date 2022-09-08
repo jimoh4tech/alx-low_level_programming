67f5faa10d7402288829914950b94a2354c8ae98
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-08 01:35:16 -0700

    Adopted betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(1, quote, 59);
+write(1, quote, 59);
 
-    return (1);
+return (1);
 
 }
