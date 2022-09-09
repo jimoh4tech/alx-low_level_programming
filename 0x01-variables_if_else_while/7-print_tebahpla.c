21ea8f2e3c6e01f51321045fb19f19f350bc7c13
Abu-Abdillah1 <63185128+Abu-Abdillah1@users.noreply.github.com>
2022-09-09 16:34:26 +0100

    Attempted task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ *
+ *
+ * Description: print letters from z to a 
+ *
+ *
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+
+{
+
+	char ch = 'z';
+
+	while (ch >= 'a')
+
+	{
+
+		putchar(ch);
+
+		--ch;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+  
+  }
