b1e4914c0114b31d080916e793114eae3d46d62d
Abu-Abdillah1 <63185128+Abu-Abdillah1@users.noreply.github.com>
2022-09-09 15:06:28 +0100

    Fixed task 1 error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n, last);
 else if (last == 0)
 printf("Last digit of %i is %i and is 0\n", n, last);
 else if (last < 6 && last != 0)
-printf("Last digit of %i is %i and is less than 6 not 0\n", n, last);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 
 return (0);
 }
