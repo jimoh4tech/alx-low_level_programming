b78f826b826403e3ba7753a1224c9106837f925a
Abu-Abdillah1 <63185128+Abu-Abdillah1@users.noreply.github.com>
2022-09-09 15:42:55 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: A simple C program that prints numbers from 0 to 9
- * 
+ *
  * Return: 0
  */
 
-int main(void) {
-    
+int main(void)
+{
+
 char num = '0';
 
 for (; num <= '9';)
