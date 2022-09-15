03d73e26aeb672b44d9eedf499119b04e3944cfd
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-14 22:56:48 -0700

    Fixed tast 1 and 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
@@ -10,30 +11,29 @@
  */
 
 void print_remaining_days(int month, int day, int year)
-  
 {
-  
-if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))    
+
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month > 2 && day >= 60)
 {
-day++;	  
+day++;
 }
-      
-printf("Day of the year: %d\n", day);      
-printf("Remaining days: %d\n", 366 - day);      
-}  
-else   
-{      
-if (month == 2 && day == 60)	
-{	  
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);	  
-}      
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
 else
-{	  
-printf("Day of the year: %d\n", day); 
-printf("Remaining days: %d\n", 365 - day);	  
-}      
+{
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
-  
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
+
 }
