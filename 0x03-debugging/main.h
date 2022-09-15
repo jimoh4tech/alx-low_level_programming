c982ad06a340fb0bbb6ecf286420c33f4897d0ec
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-14 23:16:08 -0700

    Change task 3 return type to void

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-int print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 
 
 #endif /* MAIN_H */
