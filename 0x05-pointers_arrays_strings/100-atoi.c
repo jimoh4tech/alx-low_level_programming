13a546f4b11cbb3c9e480894bc1d64e50fbb94aa
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-19 07:46:04 -0700

    Take care of -

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,20 +6,18 @@
 
 int _atoi(char *s)
 {
-int i, n = 0, sign = 0;
+int i, sign = 1;
+unsigned int n = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
+if (s[i - 1] == 45)
+sign *= -1;
 if (s[i] >= '0' && s[i] <= '9')
 {
-if (s[i - 1] == 45)
-sign = 1;
 n = n * 10 + (s[i] - 48);
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 }
 }
-
-if (sign == 1)
-n *= -1;
-return (n);
+return (n * sign);
 }
