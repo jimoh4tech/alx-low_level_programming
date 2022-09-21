e66000366e5f23787813148ed389600371db9db8
Abu-Abdillah1 <olamide14044@gmail.com>
2022-09-21 04:37:32 -0700

    Attempted some advanced task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_number - function to print the exact number
+ *
+ * @n: n of type int
+ */
+
+void print_number(int n)
+{
+unsigned int digit = n;
+
+/*first check if its negative*/
+if (n < 0)
+{
+_putchar('-');
+digit = -digit;
+}
+
+/* recursively print the first few digits*/
+if ((digit / 10) > 0)
+print_number(digit / 10);
+
+_putchar((digit % 10) + '0');
+}
