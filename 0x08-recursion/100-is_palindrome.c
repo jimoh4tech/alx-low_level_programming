c5f23eec32b76426f5a85ad0036909825e32c186
root <root@ABU-ABDILLAH.localdomain>
2022-09-28 17:38:57 +0100

    Attempted task wildcmp

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,17 +2,34 @@
 #include <string.h>
 
 /**
- * is_palindrome - check if @s is palindrome
- * @s: address of the string
- * Return: 1 if palindrome, else 0
- */
+* check_pal - check if @s is palindrome
+* @s: address of the string
+* @l: length of the string
+* @i: current index
+*
+ Return: 1 if palindrome, else 0
+*/
 
+int check_pal(char *s, int l, int i)
+{
+if (i > (l / 2))
+return (1);
+else if (s[i] != s[l - i - 1])
+return (0);
+else
+return (check_pal(s, l, i + 1));
+
+}
+
+/**
+* is_palindrome - check if @s is palindrome
+* @s: address of the string
+* Return: 1 if palindrome, else 0
+*/
 int is_palindrome(char *s)
 {
 int len = strlen(s);
-int i;
-for (i = 0; i < (len / 2); i++)
-if (s[i] != s[len - i - 1])
-return (0);
-return (1);
+int pal;
+pal = check_pal(s, len, 0);
+return (pal);
 }
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings including wildcat (*)
+ * @s1: address of s1
+ * @s2: address of s2
+ * Return: 1 if wildcat, else 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
+else if (*s1 == '\0' && *s2 == '\0')
+return (1);
+else if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+else if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+return (0);
+}
