14bff3d8333855d7cda90a1de7775f174bf6a5aa
root <root@ABU-ABDILLAH.localdomain>
2022-10-10 08:16:07 +0100

    First Attempt to C processor tasks

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Simple C program to print the name of the file
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
