263061f0fae26b1614f2540abd5d983fb0c60b95
root <root@ABU-ABDILLAH.localdomain>
2022-10-12 14:38:49 +0100

    Fixed tasks 1 and 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
+if (array != NULL && size > 0 && action != NULL)
 for (i = 0; i < size; i++)
 action(array[i]);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
+if (array != NULL && size > 0 && cmp != NULL)
+{
+
 if (array == NULL || size <= 0)
 return (-1);
 
 for (i = 0; i < size; i++)
 if (cmp(array[i]))
 return (i);
-
+}
 return (-1);
 }
