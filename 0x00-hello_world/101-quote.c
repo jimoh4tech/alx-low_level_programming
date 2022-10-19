9108d1c7ce6f9f1b5f1b5ce2b01d7dcc5426f61d
root <root@ABU-ABDILLAH.localdomain>
2022-10-19 15:19:26 +0100

    Identend codes with betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,11 @@
 
 int main(void)
 {
-char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] =
+	  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(1, quote, 59);
+	write(1, quote, 59);
 
-return (1);
+	return (1);
 
 }
