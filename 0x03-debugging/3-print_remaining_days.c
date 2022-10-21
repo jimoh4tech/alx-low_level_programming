afdad2ac9e0161e9e9a1ec8e83329ec197413ed0
root <root@ABU-ABDILLAH.localdomain>
2022-10-21 16:46:39 +0100

    Formmat code to betty linter compliance

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,28 +15,27 @@ void print_remaining_days(int month, int day, int year)
    * leap year when it can be divided
    * by 100 and 400 or by 4 evenly
    */
-  
-if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-{
-if (month > 2 && day >= 60)
-{
-day++;
-}
-      
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-     
-}
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
+
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
